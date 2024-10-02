5d5658ed0af8d1b0bcdc140c74374ac666a3741d
george$ <georgedergon@gmx.de>
2024-10-02 17:54:06 +0200

    shoppinglist.py

--- shoppinglist.py
+++ shoppinglist.py
@@ -5,8 +5,17 @@ def add_item():
    item = input("Please {insert} the article you want to add to your shoppinglist: ")
    print(f"You want to buy [Item]")
    shoppinglist.append(item)    
-   
-add_item()
 
-def show_shoppinglist():
-   item = input()
+
+#check the shopping list is empty
+def check_shoppinglist():
+   if not shoppinglist:
+      print(f" your shoppinglist is empty")
+   else:
+      print(f" your shoppinglist")
+   for item in shoppinglist:
+      print (item)
+
+
+add_item()
+check_shoppinglist()
